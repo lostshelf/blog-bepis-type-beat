f12096d0cf41b3078fb69e2a6833b571fe0308bf
Hadi Faraz <hadifaraz52@protonmail.com>
2025-04-30 02:04:08 -0600

    Add custom Context class to shut TypeScript up

--- functions/api/posts.ts
+++ functions/api/posts.ts
@@ -1,6 +1,15 @@
 /// <reference types="@cloudflare/workers-types" />
 
-export const onRequestGet async (context) => {
+interface Env {
+  BLOG_POSTS: KVNamespace;
+}
+
+interface Context {
+  request: Request;
+  env: Env;
+}
+
+export const onRequestGet = async (context: Context) => {
   const kv = (context.env as any).BLOG_POSTS as KVNamespace;
   const allPosts = await kv.list();
   const posts = await Promise.all(
