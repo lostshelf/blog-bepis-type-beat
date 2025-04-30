4a004739ab230cd6e17e54dc5410a366330d2d98
Hadi Faraz <hadifaraz52@protonmail.com>
2025-04-30 01:41:14 -0600

    Fix API route to work with cloudflare page functions

--- functions/api/posts.ts
+++ functions/api/posts.ts
@@ -1,13 +1,14 @@
-export default async function handler(req, res) {
-  const kv = process.env.BLOG_POSTS as KVNamespace;
+export const onRequestGet: PagesFunction = async (context) => {
+  const kv = context.env.BLOG_POSTS as KVNamespace;
   const allPosts = await kv.list();
-
   const posts = await Promise.all(
     allPosts.keys.map(async (key) => {
-      const data = await kv.get(key);
-      return { id: key, content: data };
+      const content = await kv.get(key);
+      return { id: key, content };
     }),
   );
 
-  res.status(200).json(posts);
-}
+  return new Response(JSON.stringify(posts), {
+    headers: { "Content-Type": "application/json" },
+  });
+};
