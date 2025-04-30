bfaa37860438ca8b35ac6ac41eaa84aecda6e3ae
Hadi Faraz <hadifaraz52@protonmail.com>
2025-04-30 01:53:38 -0600

    Pass the right variable to kv.get

--- functions/api/posts.ts
+++ functions/api/posts.ts
@@ -4,11 +4,11 @@ import type { PagesFunction } from "@cloudflare/workers-types";
 import { Response } from "@cloudflare/workers-types";
 
 export const onRequestGet: PagesFunction = async (context) => {
-  const kv = context.env.BLOG_POSTS as KVNamespace;
+  const kv = (context.env as any).BLOG_POSTS as KVNamespace;
   const allPosts = await kv.list();
   const posts = await Promise.all(
     allPosts.keys.map(async (key) => {
-      const content = await kv.get(key);
+      const content = await kv.get(key.name);
       return { id: key, content };
     }),
   );
