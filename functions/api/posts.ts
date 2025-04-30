3777f040b135cc57bbdb498424789269e0362dbc
Hadi Faraz <hadifaraz52@protonmail.com>
2025-04-30 02:00:12 -0600

    Remove type defintion for PagesFunction

--- functions/api/posts.ts
+++ functions/api/posts.ts
@@ -1,8 +1,6 @@
 /// <reference types="@cloudflare/workers-types" />
 
-import type { PagesFunction } from "@cloudflare/workers-types";
-
-export const onRequestGet: PagesFunction = async (context) => {
+export const onRequestGet async (context) => {
   const kv = (context.env as any).BLOG_POSTS as KVNamespace;
   const allPosts = await kv.list();
   const posts = await Promise.all(
